1089bb527a301bd1ec4b6920a901b7e2ae1f7cd6
youri soposki <youri.soposki@gmail.com>
2024-04-01 13:27:54 +0200

    sanitize installation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
+# app/controllers/pages_controller.rb
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @home_page_content = HomePageContent.first_or_initialize
   end
 end
+
